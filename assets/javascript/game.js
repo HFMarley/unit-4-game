95264f294271a353c48a0b8b6070f362c15a67d9
HFMarley <hankfoxmarley@gmail.com>
2018-10-29 18:42:08 -0500

    Final Version (code still needs work)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,63 +22,139 @@ $(document).ready(function () {
         return Math.floor(Math.random() * 12) + 1;
     }
     console.log("aquaGem random: " + aquaGemRand());
+    aquaGem = Number(aquaGemRand());
+
     function redGemRand() {
         return Math.floor(Math.random() * 12) + 1;
     }
     console.log("redGem random: " + redGemRand());
+    redGem = Number(redGemRand());
+
     function blueGemRand() {
         return Math.floor(Math.random() * 12) + 1;
     }
     console.log("blueGem random: " + blueGemRand());
+    blueGem = Number(blueGemRand());
+
     function greenGemRand() {
         return Math.floor(Math.random() * 12) + 1;
     }
     console.log("greenGem random: " + greenGemRand());
+    greenGem = Number(greenGemRand());
+
+    // var gemReset = function() {
+    //     aquaGem = Math.floor(Math.random() * 11)+1;
+    //     aquaGem = Number(aquaGem);
+    //     redGem = Math.floor(Math.random() * 11)+1;
+    //     redGem = Number(redGem);
+    //     blueGem = Math.floor(Math.random() * 11)+1;
+    //     blueGem = Number(blueGem);
+    //     blueGem = Math.floor(Math.random() * 11)+1;
+    //     blueGem = Number(blueGem);
+    // }
+
 
     //create a reset function for when a player wins or loses so that the values change
-    function reset() {
-        $("#computer-Number").text(computerNumber);
-        currentTotal = 0;
-        aquaGemRand();
-        redGemRand();
-        blueGemRand();
-        greenGemRand();
-    }
+    // function reset() {
+    //     $("#computer-Number").text(computerNumber);
+    //     currentTotal = 0;
+    //     aquaGemRand();
+    //     redGemRand();
+    //     blueGemRand();
+    //     greenGemRand();
+    // }
 
     //create a function to determine if a player has won or lost and then push the scores to the appropriate location 
-    var scoreChecker = function () {
+    checkScore = function () {
+        // console.log(currentTotal, computerNumber)
         if (currentTotal > computerNumber) {
-            losses++
+            losses++;
             alert("You lost! Try again");
-            $("#losses").text(losses);
-            reset();
+            $("#Losses").text(" " + losses);
             currentTotal = 0;
-        }
-
-        if (currentTotal === computerNumber) {
+            randomNumber = Math.floor(Math.random() * (120 - 19)) + 19;
+            computerNumber = Number(randomNumber);
+            $("#computer-Number").text(computerNumber);
+            function aquaGemRand() {
+                return Math.floor(Math.random() * 12) + 1;
+            }
+            console.log("aquaGem random: " + aquaGemRand());
+            aquaGem = Number(aquaGemRand());
+        
+            function redGemRand() {
+                return Math.floor(Math.random() * 12) + 1;
+            }
+            console.log("redGem random: " + redGemRand());
+            redGem = Number(redGemRand());
+        
+            function blueGemRand() {
+                return Math.floor(Math.random() * 12) + 1;
+            }
+            console.log("blueGem random: " + blueGemRand());
+            blueGem = Number(blueGemRand());
+        
+            function greenGemRand() {
+                return Math.floor(Math.random() * 12) + 1;
+            }
+            console.log("greenGem random: " + greenGemRand());
+            greenGem = Number(greenGemRand());
+        } else if (currentTotal === computerNumber) {
             wins++;
             alert("You Won! Please keep playing!");
-            $("#Wins").text(wins);
-            reset();
+            $("#Wins").text(" " + wins);
             currentTotal = 0;
+            randomNumber = Math.floor(Math.random() * (120 - 19)) + 19;
+            computerNumber = Number(randomNumber);
+            $("#computer-Number").text(computerNumber);
+            function aquaGemRand() {
+                return Math.floor(Math.random() * 12) + 1;
+            }
+            console.log("aquaGem random: " + aquaGemRand());
+            aquaGem = Number(aquaGemRand());
+        
+            function redGemRand() {
+                return Math.floor(Math.random() * 12) + 1;
+            }
+            console.log("redGem random: " + redGemRand());
+            redGem = Number(redGemRand());
+        
+            function blueGemRand() {
+                return Math.floor(Math.random() * 12) + 1;
+            }
+            console.log("blueGem random: " + blueGemRand());
+            blueGem = Number(blueGemRand());
+        
+            function greenGemRand() {
+                return Math.floor(Math.random() * 12) + 1;
+            }
+            console.log("greenGem random: " + greenGemRand());
+            greenGem = Number(greenGemRand());
         }
-    }
+    };
 
 
     //create an on click function for all of the individual crystals/gems
-    $("#aquaGem").on("click", function() {
-        $("#current-score").text(aquaGemRand + currentTotal);
-    }); 
-    $("#redGem").on("click", function() {
-        $("#current-score").text(redGemRand + currentTotal);
-    }); 
-    $("#blueGem").on("click", function() {
-        $("#current-score").text(blueGemRand + currentTotal);
-    }); 
-    $("#greenGem").on("click", function() {
-        $("#current-score").text(greenGemRand + currentTotal);
-    }); 
-    
+    $("#aquaGem").click(function () {
+        currentTotal = aquaGem + currentTotal
+        $("#current-score").text(currentTotal);
+        checkScore();
+    });
+    $("#redGem").click(function () {
+        currentTotal = redGem + currentTotal
+        $("#current-score").text(currentTotal);
+        checkScore();
+    });
+    $("#blueGem").click(function () {
+        currentTotal = blueGem + currentTotal
+        $("#current-score").text(currentTotal);
+        checkScore();
+    });
+    $("#greenGem").click(function () {
+        currentTotal = greenGem + currentTotal
+        $("#current-score").text(currentTotal);
+        checkScore();
+    });
+
 
 
 
